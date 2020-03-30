4223646e9db774a24c08e24df8ac04a461ece4ef
Nicolás Mercado <nicolas.fernando.mercado@gmail.com>
2020-03-30 11:54:21 -0300

    Added Lumen files

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Log;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    Log::info('Request received');
+    return $router->app->version();
+});
